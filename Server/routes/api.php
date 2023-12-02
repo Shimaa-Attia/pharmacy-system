aefeddd6995a270c5d301896269d5c9eb103f650
Hajar-muhammad <01120898545ha@gmail.com>
2023-12-02 17:10:03 +0200

    routs edit

--- Server/routes/api.php
+++ Server/routes/api.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 
 });
@@ -35,47 +35,52 @@ Route::post('/users',[UserController::class,'create']);
 Route::middleware('auth:api')->group(function(){
     //logout
     Route::post('/logout',[UserController::class,'logout']);
-    //update user
-    Route::put('users/{id}',[UserController::class,'update']);
-    //usersoft delete
-    Route::delete('users/delete/{id}',[UserController::class,'destroy']);
-    Route::get('users/archive',[UserController::class,'archive']);
-    Route::post('users/restore/{id}',[UserController::class,'restore']);
-    Route::delete('users/deleteArchive/{id}',[UserController::class,'deleteArchive']);
+    Route::group(['prefix'=>'users','as'=>'users.'],function(){
+       //update user
+        Route::put('/{id}',[UserController::class,'update']);
+        //usersoft delete
+        Route::delete('/delete/{id}',[UserController::class,'destroy']);
+        Route::get('/archive',[UserController::class,'archive']);
+        Route::post('/restore/{id}',[UserController::class,'restore']);
+        Route::delete('/deleteArchive/{id}',[UserController::class,'deleteArchive']);
+
+    });
+
+
+    Route::group(['prefix'=>'customers','as'=>'customers.'],function(){
+        //select all customers
+        Route::get('/',[CustomerController::class,'all']);
+        //select one customer
+        Route::get('/show/{id}',[CustomerController::class,'show']);
+        //select customer contact info
+        Route::get('/contact/{id}',[CustomerController::class,'contactInfo']);
+        //create customer
+        Route::post('/',[CustomerController::class,'store']);
+        //update customer
+        Route::put('/{id}',[CustomerController::class,'update']);
+        //customer soft delete
+        Route::delete('/delete/{id}',[CustomerController::class,'destroy']);
+        Route::get('/archive',[CustomerController::class,'archive']);
+        Route::post('/restore/{id}',[CustomerController::class,'restore']);
+        Route::delete('/deleteArchive/{id}',[CustomerController::class,'deleteArchive']);
+
+    });
+    Route::group(['prefix'=>'orders','as'=>'orders.'],function(){
+        //select all orders
+        Route::get('/',[OrdersController::class,'all']);
+        //select one order
+        Route::get('/show/{id}',[OrdersController::class,'show']);
+        //create order
+        Route::post('/',[OrdersController::class,'store']);
+        //update order
+        Route::put('/{id}',[OrdersController::class,'update']);
+        //order soft delete
+        Route::delete('/delete/{id}',[OrdersController::class,'destroy']);
+        Route::get('/archive',[OrdersController::class,'archive']);
+        Route::post('/restore/{id}',[OrdersController::class,'restore']);
+        Route::delete('/deleteArchive/{id}',[OrdersController::class,'deleteArchive']);
+
+    });
 
 });
 
-Route::group(['prefix'=>'customers','as'=>'customers.'],function(){
-    //select all customers
-    Route::get('/',[CustomerController::class,'all']);
-    //select one customer
-    Route::get('/show/{id}',[CustomerController::class,'show']);
-    //select customer contact info
-    Route::get('/contact/{id}',[CustomerController::class,'contactInfo']);
-    //create customer
-    Route::post('/',[CustomerController::class,'store']);
-    //update customer
-    Route::put('/{id}',[CustomerController::class,'update']);
-    //customer soft delete
-    Route::delete('/delete/{id}',[CustomerController::class,'destroy']);
-    Route::get('/archive',[CustomerController::class,'archive']);
-    Route::post('/restore/{id}',[CustomerController::class,'restore']);
-    Route::delete('/deleteArchive/{id}',[CustomerController::class,'deleteArchive']);
-
-});
-Route::group(['prefix'=>'orders','as'=>'orders.'],function(){
-    //select all orders
-    Route::get('/',[OrdersController::class,'all']);
-    //select one order
-    Route::get('/show/{id}',[OrdersController::class,'show']);
-    //create order
-    Route::post('/',[OrdersController::class,'store']);
-    //update order
-    Route::put('/{id}',[OrdersController::class,'update']);
-    //order soft delete
-    Route::delete('/delete/{id}',[OrdersController::class,'destroy']);
-    Route::get('/archive',[OrdersController::class,'archive']);
-    Route::post('/restore/{id}',[OrdersController::class,'restore']);
-    Route::delete('/deleteArchive/{id}',[OrdersController::class,'deleteArchive']);
-
-});
