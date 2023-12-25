2b2bb2392aa78202fa42d1bd20a47853c623e790
Hajar-muhammad <01120898545ha@gmail.com>
2023-12-26 01:55:59 +0200

    time zone

--- Server/app/Http/Resources/SalePointRecource.php
+++ Server/app/Http/Resources/SalePointRecource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use App\Models\Order;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
@@ -16,10 +17,11 @@ class SalePointRecource extends JsonResource
     public function toArray(Request $request): array
     {
         $todayDate = date("Y-m-d");
+        // $todayDate = Carbon::now();;
           $orders =  Order::where('sale_point_id', $this->id)
          ->where('created_at', 'like', "$todayDate%")
           ->get();
-          $total = 0;
+          $total =0;
           foreach ($orders as $order){
             $total += $order->cost;
           }
